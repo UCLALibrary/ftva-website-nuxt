e6a4b5fbd8dcac6d1bb1e5764d34e26f3e66d6f5
pghorpade <pghorpade@library.ucla.edu>
2024-06-05 08:52:28 -0700

    fix: eslint errors and cypress test

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -16,65 +16,7 @@ describe('Website Homepage', () => {
     cy.get('a.logo-ucla')
       .should('have.attr', 'href', 'https://www.library.ucla.edu')
     // NavPrimary
-    cy.get('.nav-primary').then((elem) => {
-      if (elem[0].textContent.startsWith('Modern Endangered Archives Program Get help with')) {
-        cy.get('.nav-primary').should('contain', 'Who we are')
-          .and('contain', 'For applicants')
-          .and('contain', 'Funded projects')
-      }
-    })
-
-    // cy.get("h2.visually-hidden").should(
-    //     "contain",
-    //     "Featured Projects"
-    // )
-
-    /* TODO enable later after home page is done start
-        cy.get("h2.section-title").should(
-            "contain",
-            "Program Resources"
-        )
-
-        cy.get("h2.section-title").should(
-            "contain",
-            "News"
-        )
-
-        cy.contains("a.smart-link", "Browse Collections").should("have.attr", "target", "_blank")
-*/ // TODO enable this later end
-    // MastheadPrimary
-    // cy.get(".masthead-primary").find(".logo").should("be.visible")
-    // cy.get(".masthead-primary")
-    //     .find(".search-home")
-    //     .should("be.visible")
-    //     .and("contain", "Search the Library Site")
-    //     .and("contain", "Search Materials")
-    // cy.get(".masthead-primary")
-    //     .find("input.input-search")
-    //     .should("have.attr", "placeholder", "Search by keyword")
-    // cy.get(".masthead-primary")
-    //     .contains("a", "Course Reserves")
-    //     .should(
-    //         "have.attr",
-    //         "href",
-    //         "https://catalog.library.ucla.edu/vwebv/enterCourseReserve.do"
-    //    )
-    // cy.get(".masthead-primary")
-    //     .contains("a", "UCLA Research Guides")
-    //     .should("have.attr", "href", "https://guides.library.ucla.edu/")
-    // cy.get(".masthead-primary")
-    //     .contains("a", "Databases A-Z")
-    //     .should(
-    //         "have.attr",
-    //         "href",
-    //         "https://guides.library.ucla.edu/az.php"
-    //     )
-
-    // get help with
-    // cy.get(".section-cards-with-illustrations").should(
-    //     "contain",
-    //     "Get Help With"
-    // )
+    
 
     cy.percySnapshot({ widths: [768, 992, 1200] })
   })
