06e07ce98de3bfff4f630d7c25b09447e6962a25
pghorpade <pghorpade@library.ucla.edu>
2024-07-29 15:37:13 -0700

    ci: fix cypress event detail spec file name and assertions

--- cypress/e2e/eventDetailPage.cy.js
+++ cypress/e2e/eventDetailPage.cy.js
@@ -2,7 +2,7 @@ describe('Event Detail page', () => {
   it('Visit a event Detail Page', () => {
     cy.visit('/events/la-région-centrale-03-08-24')
 
-    cy.get('h1.title').should('contain', 'TEST - La Région Centrale')
+    cy.get('h3.title-no-link').should('contain', 'TEST - La Région Centrale')
 
     cy.percySnapshot({ widths: [768, 992, 1200] })
   })
--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -5,14 +5,14 @@ describe('Website Homepage', () => {
 
     // UCLA brand
     // TODO Un comment this once header and footer are added back to default layout file.
-    /*cy.get('.site-brand-bar').should('be.visible')
+    /* cy.get('.site-brand-bar').should('be.visible')
     cy.get('.visually-hidden').should('contain', 'UCLA Home')
     cy.get('.ucla-logo')
       .parent()
       .should('have.attr', 'href', 'https://www.ucla.edu')
     cy.get('.svg__logo-library')
       .parent()
-      .should('have.attr', 'href', 'https://www.library.ucla.edu')*/
+      .should('have.attr', 'href', 'https://www.library.ucla.edu') */
 
     // cy.get('a.logo-ucla')
     //   .should('have.attr', 'href', 'https://www.library.ucla.edu')
