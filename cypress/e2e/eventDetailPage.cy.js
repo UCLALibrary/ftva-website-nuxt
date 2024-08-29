5d13a9f385de829fb53eaf7d6ecc957ea23e2bdb
jendiamond <thejendiamond@gmail.com>
2024-08-29 00:26:02 -0700

    add wait time

--- cypress/e2e/eventDetailPage.cy.js
+++ cypress/e2e/eventDetailPage.cy.js
@@ -8,6 +8,7 @@ describe('Event Detail page', () => {
   context('breadcrumb', () => {
     // NavBreadcrumb
     it('has a breadcrumb nav with the correct title', () => {
+      cy.wait(1000)
       cy.getByData('breadcrumb').contains('TEST - La Région Centrale')
     })
   })
@@ -21,11 +22,11 @@ describe('Event Detail page', () => {
     //   cy.getByData('screening-details').contains('Trailer with Cover image')
     // })
   })
-// modal exists
-// two images
-// creditText is visible .contains('Movie Database')
-// click on arrow and it moves to next image
-// cy.getByData("ticket-info").find("a").contains("Plan Your Visit").click()
+  // modal exists
+  // two images
+  // creditText is visible .contains('Movie Database')
+  // click on arrow and it moves to next image
+  // cy.getByData("ticket-info").find("a").contains("Plan Your Visit").click()
 
   context('Metablock', () => {
     // CardMeta
@@ -51,52 +52,52 @@ describe('Event Detail page', () => {
     })
   })
 
-    context('EventDescription', () => {
-      // RichText - EventDescription
-      it('has an event description', () => {
-        cy.getByData('event-description').contains('The late avant-garde master Michael Snow')
-      })
+  context('EventDescription', () => {
+    // RichText - EventDescription
+    it('has an event description', () => {
+      cy.getByData('event-description').contains('The late avant-garde master Michael Snow')
     })
+  })
 
-    context('Acknowledgements', () => {
-      // RichText - Acknowledgements
-      it('has an acknowledgement', () => {
-        cy.getByData('acknowledgements').contains('Special thanks to our community partner')
-      })
+  context('Acknowledgements', () => {
+    // RichText - Acknowledgements
+    it('has an acknowledgement', () => {
+      cy.getByData('acknowledgements').contains('Special thanks to our community partner')
     })
+  })
 
-    context('Section Screening Details', () => {
-      it('has a category', () => {
-        cy.getByData('screening-details').contains ('Screening 1 of 5')
-      })
-      it('has a title', () => {
-        cy.getByData('screening-details').contains('Trailer with Cover image')
-      })
-      it('has the Year', () => {
-        cy.getByData('screening-details').contains ('Year 2025')
-      })
-      it('has the Country', () => {
-        cy.getByData('screening-details').contains('Country South Country')
-      })
-      it('has the Language', () => {
-        cy.getByData('screening-details').contains ('Language Spanglish')
-      })
-      it('has the Runtime', () => {
-        cy.getByData('screening-details').contains('Runtime 190 minutes')
-      })
-      it('has Tag Labels', () => {
-        cy.getByData('screening-details').contains ('IMAX')
-      })
-      it('has Tag Labels', () => {
-        cy.getByData('screening-details').contains ('Experimental Film')
-      })
-      it('has a Description', () => {
-        cy.getByData('screening-details').contains('MICHAEL SNOW’S 1970–71 FILM La Région Centrale')
-      })
-      // it('has a Trailer', () => {
-      //   cy.getByData('screening-details').contains('La Région Centrale is three long hours')
-      // })
+  context('Section Screening Details', () => {
+    it('has a category', () => {
+      cy.getByData('screening-details').contains('Screening 1 of 5')
     })
+    it('has a title', () => {
+      cy.getByData('screening-details').contains('Trailer with Cover image')
+    })
+    it('has the Year', () => {
+      cy.getByData('screening-details').contains('Year 2025')
+    })
+    it('has the Country', () => {
+      cy.getByData('screening-details').contains('Country South Country')
+    })
+    it('has the Language', () => {
+      cy.getByData('screening-details').contains('Language Spanglish')
+    })
+    it('has the Runtime', () => {
+      cy.getByData('screening-details').contains('Runtime 190 minutes')
+    })
+    it('has Tag Labels', () => {
+      cy.getByData('screening-details').contains('IMAX')
+    })
+    it('has Tag Labels', () => {
+      cy.getByData('screening-details').contains('Experimental Film')
+    })
+    it('has a Description', () => {
+      cy.getByData('screening-details').contains('MICHAEL SNOW’S 1970–71 FILM La Région Centrale')
+    })
+    // it('has a Trailer', () => {
+    //   cy.getByData('screening-details').contains('La Région Centrale is three long hours')
+    // })
+  })
 
   // SIDEBAR / DETAILS
   context('BlockEventDetail', () => {
@@ -112,12 +113,12 @@ describe('Event Detail page', () => {
     })
   })
 
-  // context('Calendar Dropdown', () => {
-  //   it.only('has a calendar dropdown', () => {
-  //     cy.wait(1000)
-  //     cy.getByData('calendar-dropdown').contains('Add to Calendar')
-  //   })
-  // })
+  context('Calendar Dropdown', () => {
+    it.only('has a calendar dropdown', () => {
+      cy.wait(1000)
+      cy.getByData('calendar-dropdown').contains('Add to Calendar')
+    })
+  })
 
   context('BlockInfo/ Ticket Info', () => {
     it('has ticket information admission', () => {
@@ -155,20 +156,16 @@ describe('Event Detail page', () => {
     it('has the Footer Sock', () => {
       cy.getByData('footer').contains('© 2024 Regents of the University of California')
     })
+  })
+})
+
     // https://docs.cypress.io/guides/end-to-end-testing/writing-your-first-end-to-end-test
     // it.only('allows users to subscribe to the email list', () => {
     //   cy.getByData('email-input').type('human@gmail.com')
     //   cy.getByData('submit-button').click()
     //   cy.getByData('success-message').should('exist').contains('human@gmail.com')
     // })
-  })
 
-  // ResponsiveImage (This page has imageCarousel)
-  // it('has a single responsive image', () =>{
-  //   cy.getByData('single-image').contains('TEST - La Région Centrale')
-  // })
-
-  // context('other stuff', () => {
   // UI Elements: Test visibility and functionality of important UI elements (e.g., buttons, links, modals)
 
   // Verify that data is displayed correctly, especially dynamic data fetched from APIs
@@ -185,19 +182,16 @@ describe('Event Detail page', () => {
   // cy.percySnapshot('eventdetailpage', { widths: [768, 992, 1200] })
   // })
 
+  // ---------
 
-// ---------
+  // UI Elements: Test visibility and functionality of important UI elements (e.g., buttons, links, modals)
 
-// UI Elements: Test visibility and functionality of important UI elements (e.g., buttons, links, modals)
+  // Verify that data is displayed correctly, especially dynamic data fetched from APIs
 
-// Verify that data is displayed correctly, especially dynamic data fetched from APIs
+  // Event Information Display: Ensure that all event details (title, description, date, time, location) are displayed correctly
 
-// Event Information Display: Ensure that all event details (title, description, date, time, location) are displayed correctly
+  // Add to Calendar: Verify that the "Add to Calendar" button works as expected
 
-// Add to Calendar: Verify that the "Add to Calendar" button works as expected
+  // Responsive Design: Check that the page renders correctly on different screen sizes
 
-// Responsive Design: Check that the page renders correctly on different screen sizes
-
-// Navigation: Ensure that navigation to and from the event detail page works smoothly
-
-})
+  // Navigation: Ensure that navigation to and from the event detail page works smoothly
