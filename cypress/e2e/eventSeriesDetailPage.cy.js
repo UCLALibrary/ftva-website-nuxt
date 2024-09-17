4051bba9b661483f1b3fef91c7cea9832ee503dc
pghorpade <pghorpade@library.ucla.edu>
2024-09-16 18:49:23 -0700

    tests: add percy name for the the snapshot

--- cypress/e2e/eventSeriesDetailPage.cy.js
+++ cypress/e2e/eventSeriesDetailPage.cy.js
@@ -2,6 +2,6 @@ describe('Event Series Detail page', () => {
   it('Visit the Homepage', () => {
     cy.visit('/series/step-up-series')
 
-    cy.percySnapshot({ widths: [768, 992, 1200] })
+    cy.percySnapshot('eventseriesdetailpage',{ widths: [768, 992, 1200] })
   })
 })
