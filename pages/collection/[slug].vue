1a56c1c9e70212780694cdd0fa623f7ae984a191
Jess Divers <jessica.divers@library.ucla.edu>
2024-10-04 10:07:30 -0700

    feat: start collection gql

---
+++ pages/collection/[slug].vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+// COMPONENT RE-IMPORTS
+// TODO: remove when we have implemented component library as a module
+// https://nuxt.com/docs/guide/directory-structure/components#library-authors
+import { CardMeta, DividerWayFinder, FlexibleMediaGalleryNewLightbox, NavBreadcrumb, ResponsiveImage, RichText, SectionTeaserCard, SectionWrapper, TwoColLayoutWStickySideBar } from 'ucla-library-website-components'
+
+// HELPERS
+import _get from 'lodash/get'
+
+// GQL
+import FTVACollectionDetail from '../gql/queries/FTVACollectionDetail.gql'
+
+const { $graphql } = useNuxtApp()
+
+const route = useRoute()
+
+</script>
+<template>
+  <main
+    id="main"
+    class="page page-collection-detail"
+  >
+  </main>
+</template>
+<style lang="scss" scoped>
+.page-collection-detail {
+  position: relative;
+
+  // TODO move this element to global mixin or something
+  &:before {
+    content: '';
+    position: absolute;
+    background-color: var(--pale-blue);
+    aspect-ratio: 1440 / 520;
+    max-height: 518px; //prevent overflow on large screens
+    min-height: 225px; //prevent too much shrinking on small screens
+    width: 100%;
+    z-index: -1;
+  }
+
+  // TODO global?
+  .one-column {
+    width: 100%;
+    max-width: var(--max-width);
+    margin: 0 auto;
+
+    :deep(.nav-breadcrumb) {
+      padding: 0px;
+    }
+  }
+
+  // TODO global?
+  .full-width {
+    width: 100%;
+    background-color: var(--pale-blue);
+    margin: 0 auto;
+
+    .section-wrapper.theme-paleblue {
+      background-color: var(--pale-blue);
+    }
+  }
+}
+</style>
