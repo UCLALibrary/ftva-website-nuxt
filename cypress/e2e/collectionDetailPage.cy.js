3b0b2dc7cd8fe5d43663d99c2c769c6ab2b2a485
Jess Divers <jessica.divers@library.ucla.edu>
2024-10-15 11:14:57 -0700

    chore: tests

--- cypress/e2e/collectionDetailPage.cy.js
+++ cypress/e2e/collectionDetailPage.cy.js
@@ -1,9 +1,11 @@
 describe('Collection Detail Page', () => {
   it('Visits a Collection Detail Page', () => {
-    cy.visit('/collections/test-collection-two')
+    cy.visit('/collections/test-get-used-to-it')
     cy.getByData('breadcrumb').should('be.visible')
     // sharebutton should be visible
+    cy.getByData('share-button').should('be.visible')
     // cta should be in sidebar w specific data
+    cy.getByData('sidebar-cta').should('be.visible')
     cy.percySnapshot('collectiondetailpage', { widths: [768, 992, 1200] })
   })
 })
