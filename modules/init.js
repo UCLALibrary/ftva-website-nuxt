69c45c00398d0e770c5541071a00c735dc530b2b
Parinita Mulak <pghorpade@library.ucla.edu>
2024-12-11 11:28:54 -0800

    feat: APPS-3076 Add typescript and SectionRemoveSearchFilters (#82)
    * feat: update component library version to get the refactored sectionremovesearchfietsr and add typescript to the listing page
    
    * fix: eslint errors
    
    * fix: null date  becoming 1969 date
    
    * fix: eslint errors
    
    * fix: eslint errors
    
    * fix: eslint errors
    
    * fix: Add background color

--- modules/init.js
+++ modules/init.js
@@ -3,14 +3,14 @@ import fetch from 'node-fetch'
 export default defineNuxtModule({
 
   setup(options, nuxt) {
-    console.log('Nuxt module start ')
-    console.log('Is the environement local Dev' + import.meta.dev)
+    // console.log('Nuxt module start ')
+    // console.log('Is the environement local Dev' + import.meta.dev)
     if (!nuxt.options._prepare && !import.meta.dev) {
       nuxt.hooks.hook('nitro:init', async (nitro) => {
-        console.log('Ready to create library temp index...')
+        // console.log('Ready to create library temp index...')
 
         const esLibraryIndexTemp = nuxt.options.runtimeConfig.public.esTempIndex
-        console.log('Index named:' + esLibraryIndexTemp)
+        // console.log('Index named:' + esLibraryIndexTemp)
         // https://www.elastic.co/guide/en/elasticsearch/reference/current/flattened.html
         try {
           const response = await fetch(`${nuxt.options.runtimeConfig.public.esURL}/${esLibraryIndexTemp}`, {
@@ -48,8 +48,8 @@ export default defineNuxtModule({
           })
           const body = await response.text()
           const testJson = JSON.parse(body)
-          console.log('Index created:' + JSON.stringify(testJson))
-          console.log('Elastic Search index created succesfully!')
+          // console.log('Index created:' + JSON.stringify(testJson))
+          // console.log('Elastic Search index created succesfully!')
         } catch (err) {
           console.error('Error:', err)
           console.error('Response body:', body)
@@ -57,6 +57,6 @@ export default defineNuxtModule({
         }
       })
     }
-    console.log('Nuxt module end ')
+    // console.log('Nuxt module end ')
   }
 })
