3431413a5c44d7923bd44c87490001d379982d75
Jess Divers <jessica.divers@library.ucla.edu>
2025-01-03 12:05:04 -0700

    fix: restore config

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   defaultCommandTimeout: 60000,
   video: false,
-  experimentalMemoryManagement: true,
-  numTestsKeptInMemory: 0,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
