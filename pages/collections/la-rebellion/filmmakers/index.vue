00879db130b06a858e0836cb495ed4927173990f
Jess Divers <jessica.divers@library.ucla.edu>
2025-01-21 15:43:05 -0700

    feat: rough index page

--- pages/collections/la-rebellion/filmmakers/index.vue
+++ pages/collections/la-rebellion/filmmakers/index.vue
@@ -1,66 +1,70 @@
 <script setup>
 // COMPONENTS
-import { DividerGeneral, SectionWrapper } from 'ucla-library-website-components'
+import { DividerGeneral, SectionWrapper, RichText } from 'ucla-library-website-components'
+import { computed } from 'vue'
 
 // HELPERS
 import _get from 'lodash/get'
 
 // GQL
-// TODO
+import FTVALARebellionFilmmakersList from '~/gql/queries/FTVALARebellionFilmmakersList.gql'
 
 const { $graphql } = useNuxtApp()
 
-// const { data, error } = await useAsyncData('collection-list', async () => {
-//   const data = await $graphql.default.request(FTVACollectionList)
-//   return data
-// })
+const { data, error } = await useAsyncData('la-rebellion-filmmakers', async () => {
+  const data = await $graphql.default.request(FTVALARebellionFilmmakersList)
+  console.log('data', data)
+  return data
+})
 
-// if (error.value) {
-//   throw createError({
-//     ...error.value, statusMessage: 'Page not found.' + error.value, fatal: true
-//   })
-// }
+if (error.value) {
+  throw createError({
+    ...error.value, statusMessage: 'Page not found.' + error.value, fatal: true
+  })
+}
 
-// if (!data.value.entries) {
-//   // console.log('no data')
-//   throw createError({
-//     statusCode: 404,
-//     statusMessage: 'Page Not Found',
-//     fatal: true
-//   })
-// }
+if (!data.value.entries) {
+  // console.log('no data')
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found',
+    fatal: true
+  })
+}
 
-// const page = ref(_get(data.value, 'entries[0]', {}))
-// useHead({
-//   title: page.value?.title || '... loading',
-// })
+const page = ref(_get(data.value, 'entry', {}))
+
+const showSummary = computed(() => {
+  return page.value?.summary && page.value?.displaySummary === 'yes'
+})
+useHead({
+  title: page.value?.title || '... loading',
+})
 </script>
 
 <template>
   <div
-    class="page page-collections"
+    class="page page-filmmakers"
     style="padding: 25px 100px;"
   >
     <section-wrapper section-title="LA Rebellion Filmmakers">
-      <h2>SEACHBAR</h2>
-
-      <!-- <div
-        v-for="collection in data.entries"
-        :key="collection?.id"
+      <template v-if="showSummary">
+        <RichText :rich-text-content="page.summary" />
+      </template>
+      <div
+        v-for="filmmaker in data.entries"
+        :key="filmmaker?.id"
       >
-        <NuxtLink :to="`/${collection?.to}`">
-          {{ collection?.title }}
+        <NuxtLink :to="`${filmmaker?.to}`">
+          {{ filmmaker?.title }}
         </NuxtLink> <br>
-        <h4>ftvaCollectionType: <code>{{ collection?.ftvaCollectionType }}</code></h4>
-        <h4>richText: <code>{{ collection?.richText }}</code></h4>
-        <h4>videoEmbed: <code>{{ collection?.videoEmbed }}</code></h4>
-        <h4>viewAllSectionLink: <code>{{ collection?.viewAllSectionLink }}</code></h4>
-        <h4>infoBlock: <code>{{ collection?.infoBlock }}</code></h4>
-        <h4>image: <code>{{ collection?.image }}</code></h4>
+        <h4>to: <code>{{ filmmaker?.to }}</code></h4>
+        <h4>richText: <code>{{ filmmaker?.richText }}</code></h4>
+        <h4>associatedFilms: <code>{{ filmmaker?.associatedFilms }}</code></h4>
         <divider-general />
-      </div> -->
+      </div>
 
-      <!-- <code><strong>PAGE</strong> {{ page }}</code> -->
+      <code><strong>PAGE</strong> {{ page }}</code>
     </section-wrapper>
   </div>
 </template>
