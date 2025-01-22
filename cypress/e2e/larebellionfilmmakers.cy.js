e0dd9a72a9bec2145017327992422ccc1b4128d1
Jess Divers <jessica.divers@library.ucla.edu>
2025-01-22 16:46:30 -0700

    fix: update test

--- cypress/e2e/larebellionfilmmakers.cy.js
+++ cypress/e2e/larebellionfilmmakers.cy.js
@@ -1,6 +1,6 @@
 describe('Filmmakers Detail Page', () => {
-  it('Shows a specific filmmakers profile based on slug', () => {
-    cy.visit('/collections/la-rebellion/filmmakers/bernard-nicolas')
+  it('Visits the LA Rebellion Filmmaker Detail page', () => {
+    cy.visit('/collections/la-rebellion/filmmakers/test-person')
 
     cy.percySnapshot('larebellionfilmmakersdetail', { widths: [768, 992, 1200] })
   })
