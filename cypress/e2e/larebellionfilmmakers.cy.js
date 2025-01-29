acfd5a83f6855d654894a347ead1788f7e8d897d
Jess Divers <jessica.divers@library.ucla.edu>
2025-01-29 15:07:14 -0700

    fix: update cypress test

--- cypress/e2e/larebellionfilmmakers.cy.js
+++ cypress/e2e/larebellionfilmmakers.cy.js
@@ -1,6 +1,6 @@
 describe('Filmmakers Detail Page', () => {
   it('Visits the LA Rebellion Filmmaker Detail page', () => {
-    cy.visit('/collections/la-rebellion/filmmakers/test-person')
+    cy.visit('/collections/l-a-rebellion/filmmakers/test-person')
 
     cy.percySnapshot('larebellionfilmmakersdetail', { widths: [768, 992, 1200] })
   })
