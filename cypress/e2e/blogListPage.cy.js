8fe0852dca187fc3231152cd3dddc8961eaca86c
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2025-01-30 12:17:05 -0800

    feat: APPS 3110 Assemble Blog Listing page (#101)
    * wip
    
    * wip
    
    * wip: update logic for data images; adjust page styling
    
    * fix: parse article categories
    
    * wip: fix infinite scrolling
    
    * task: code cleanup
    
    * update: add displaySummary field to article gql
    
    * test: add basic cypress test
    
    * wip: add pageSummary toggle logic
    
    * task: update component library to 3.45.0
    
    * add BlockCard ftva variant
    
    * fix: linting, remove v-html directive
    
    * update library package to 3.45.1
    
    * update CSS after component pkg update
    
    * Add more conditions to cypress test
    
    * fix: remove custom rich-text padding-right
    
    * remove console statements
    
    * fix: ux/css  updates
    
    * update query to incl. categories for featured artciles
    
    * fix: display featured articles' categories
    
    * fix: add page SectionHeaders with h3
    
    * fix: remove unneeded css values
    
    * update featured blogs section title on mobile
    
    * update featuredArticles query with alternativeTitle field
    
    * add logic to parse alternate richtext featuredArticle title
    
    * update preview watcher to track pageTitle
    
    ---------
    
    Co-authored-by: tinuola <tinuola@users.noreply.github.com>
    Co-authored-by: JenDiamond <thejendiamond@gmail.com>

---
+++ cypress/e2e/blogListPage.cy.js
@@ -0,0 +1,31 @@
+Cypress.on('uncaught:exception', () => { return false })
+
+describe('Blog Listing page', () => {
+  beforeEach(() => {
+    cy.visit('/blog')
+  })
+
+  it('Visits Blog Listing page', () => {
+    cy.getByData('blog-page-title').should('be.visible')
+
+    cy.getByData('featured-blog-0').should('be.visible')
+
+    cy.getByData('featured-blog-1').should('be.visible')
+
+    cy.getByData('featured-blog-2').should('be.visible')
+
+    cy.getByData('latest-blogs').should('be.visible')
+
+    cy.percySnapshot('eventslistpage', { widths: [768, 992, 1200] })
+  })
+
+  it('Shows only one featured blog in mobile view', () => {
+    cy.viewport(750, 720)
+
+    cy.getByData('featured-blog-0').should('be.visible')
+
+    cy.getByData('featured-blog-1').should('not.be.visible')
+
+    cy.getByData('featured-blog-2').should('not.be.visible')
+  })
+})
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,6 +4,7 @@ import { SectionWrapper } from 'ucla-library-website-components'
 
 // HELPERS
 import _get from 'lodash/get'
+import { useWindowSize, useInfiniteScroll } from '@vueuse/core'
 
 // GQL
 import FTVAArticleList from '../gql/queries/FTVAArticleList.gql'
@@ -12,6 +13,8 @@ import FTVAArticleList from '../gql/queries/FTVAArticleList.gql'
 import { useContentIndexer } from '~/composables/useContentIndexer'
 
 const { $graphql } = useNuxtApp()
+
+const route = useRoute()
 
 const { data, error } = await useAsyncData('article-list', async () => {
   const data = await $graphql.default.request(FTVAArticleList)
@@ -24,7 +27,7 @@ if (error.value) {
   })
 }
 
-if (!data.value.entries) {
+if (!data.value.entry) {
   // console.log('no data')
   throw createError({
     statusCode: 404,
@@ -48,26 +51,212 @@ if (data.value.entry && import.meta.prerender) {
 
 // DATA
 const page = ref(_get(data.value, 'entry', {}))
+const pageTitle = page.value.title
+const pageSummary = page.value.summary
 const featuredArticles = page.value.ftvaFeaturedArticles
-const articleList = ref(_get(data.value, 'entries', {}))
-console.log('Data: ', data.value)
+
+// PREVIEW WATCHER FOR CRAFT CONTENT
+watch(data, (newVal, oldVal) => {
+  // console.log('In watch preview enabled, newVal, oldVal', newVal, oldVal)
+  page.value = _get(newVal, 'entry', {})
+  pageTitle.value = page.value.title
+  pageSummary.value = page.value.summary
+  featuredArticles.value = page.value.ftvaFeaturedArticles
+})
+
+// "STATE"
+const desktopPage = useState('desktopPage', () => 1) // Persist desktop page
+const desktopArticles = ref([]) // Desktop articles list
+const mobileArticles = ref([]) // Mobile articles list
+const articles = computed(() => (isMobile.value ? mobileArticles.value : desktopArticles.value))
 
 const currentPage = ref(1)
 const documentsPerPage = 10
+const totalPages = ref(0)
 
-// TEST
-const { paginatedArticlesQuery } = useArticlesListSearch()
+// INFINITE SCROLLING
+const isLoading = ref(false)
+const isMobile = ref(false)
+const hasMore = ref(true) // Flag to control infinite scroll
 
-onMounted(async () => {
-  const esOutput = await paginatedArticlesQuery(
-    currentPage.value,
-    documentsPerPage,
-    'postDate',
-    'asc'
-  )
+const scrollElem = ref(null)
+const { reset } = useInfiniteScroll(
+  scrollElem,
+  async () => {
+    if (isMobile.value && hasMore.value && !isLoading.value) {
+      currentPage.value++
+      await searchES()
+    }
+  },
+  { distance: 100 }
+)
 
-  console.log('ES output total hits: ', esOutput.hits.total.value) // 445
+// HANDLE WINDOW SIZING
+const { width } = useWindowSize()
+watch(width, (newWidth) => {
+  const wasMobile = isMobile.value
+
+  isMobile.value = newWidth <= 750
+  // Reinitialize only when transitioning between mobile and desktop
+  if (wasMobile !== isMobile.value) {
+    handleScreenTransition()
+  }
+}, { immediate: true })
+
+// HANDLE SCREEN TRANSITIONS
+function handleScreenTransition() {
+  if (isMobile.value) {
+    // Switching to mobile: save desktop page, clear query param
+
+    desktopPage.value = currentPage.value
+    currentPage.value = 1
+    mobileArticles.value = []
+    hasMore.value = true
+    const { page, ...remainingQuery } = route.query
+    useRouter().push({ query: { ...remainingQuery } })
+  } else {
+    // Switching to desktop: restore query param
+    if (totalPages.value === 1)
+      desktopPage.value = 1
+    const restoredPage = desktopPage.value || 1
+    useRouter().push({ query: { ...route.query, page: restoredPage.toString() } })
+    currentPage.value = restoredPage
+    desktopArticles.value = []
+  }
+  searchES()
+}
+
+// ELASTIC SEARCH
+async function searchES() {
+  if (isLoading.value || !hasMore.value) return
+
+  isLoading.value = true
+
+  // COMPOSABLE
+  const { paginatedArticlesQuery } = useArticlesListSearch()
+
+  try {
+    const results = await paginatedArticlesQuery(
+      currentPage.value,
+      documentsPerPage,
+      'postDate',
+      'desc',
+      ['*']
+    )
+
+    if (results && results.hits && results?.hits?.hits?.length > 0) {
+      const newArticles = results.hits.hits || []
+
+      if (isMobile.value) {
+        totalPages.value = 0
+
+        mobileArticles.value.push(...newArticles)
+
+        hasMore.value = currentPage.value < Math.ceil(results.hits.total.value / documentsPerPage)
+      } else {
+        desktopArticles.value = newArticles
+
+        totalPages.value = Math.ceil(results.hits.total.value / documentsPerPage)
+      }
+    } else {
+      totalPages.value = 0
+
+      hasMore.value = false
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching data:', error)
+    hasMore.value = false
+  } finally {
+    isLoading.value = false
+  }
+}
+
+watch(
+  () => route.query,
+  (newVal, oldVal) => {
+    isLoading.value = false
+    currentPage.value = route.query.page ? parseInt(route.query.page) : 1
+    isMobile.value ? mobileArticles.value = [] : desktopArticles.value = []
+    hasMore.value = true
+    searchES()
+  }, { deep: true, immediate: true }
+)
+
+// PAGE SUMMARY
+const showPageSummary = computed(() => {
+  return page.value?.summary && page.value?.displaySummary === 'yes'
 })
+
+// PARSED FEATURED ARTICLES
+const parsedFeaturedArticles = computed(() => {
+  if (featuredArticles.length === 0) {
+    return
+  }
+
+  return featuredArticles.map((obj) => {
+    const parsedTitle = parseRichTextTitle(obj)
+
+    return {
+      image: obj.ftvaImage[0],
+      to: obj.uri,
+      title: parsedTitle,
+      category: parseArticleCategories(obj.articleCategories),
+      text: obj.ftvaHomepageDescription,
+      dateCreated: obj.postDate
+    }
+  })
+})
+
+// PARSED ARTICLE LIST
+const parsedArticles = computed(() => {
+  if (articles.value.length === 0) return []
+
+  return articles.value.map((obj) => {
+    return {
+      ...obj._source,
+      to: `/${obj._source.uri}`,
+      title: obj._source.title,
+      category: parseArticleCategories(obj._source.articleCategories),
+      description: obj._source.aboutTheAuthor,
+      startDate: obj._source.postDate,
+      endDate: obj._source.postDate,
+      image: isImageExists(obj)
+    }
+  })
+})
+
+// GET FEATURED ARTICLES RICH TEXT TITLE
+function parseRichTextTitle(obj) {
+  return !obj.ftvaAlternativeTitle ? obj.title : obj.ftvaAlternativeTitle
+}
+
+// GET ARTICLE CATEGORIES
+function parseArticleCategories(arr) {
+  if (arr.length === 0) return
+  return arr.map(obj => obj.title).join(', ')
+}
+
+// GET IMAGE
+function parsedCarouselImage(obj) {
+  return obj._source.imageCarousel
+}
+
+function parsedFTVAImage(obj) {
+  return obj._source.ftvaImage
+}
+
+function isImageExists(obj) {
+  // Use FTVA Image
+  if (parsedFTVAImage(obj) && parsedFTVAImage(obj).length === 1) {
+    return parsedFTVAImage(obj)[0]
+  } else if (parsedCarouselImage(obj) && parsedCarouselImage(obj).length >= 1) {
+    // Use ImageCarousel
+    return parsedCarouselImage(obj)[0]
+  } else {
+    return null
+  }
+}
 
 useHead({
   title: page.value ? page.value.title : '... loading',
@@ -82,33 +271,201 @@ useHead({
 </script>
 
 <template>
-  <div
-    class="page page-article-list"
-    style="padding: 25px 100px;"
-  >
-    <section-wrapper section-title="Archive Blog">
-      <!--<div
-        v-for="article in data.entries"
-        :key="article?.id"
-      >
-        <NuxtLink :to="`/blog/${article?.to}`">
-          {{ article?.title }}
-        </NuxtLink>
+  <div class="page page-article-list">
+    <SectionWrapper
+      ref="scrollElem"
+      :section-title="pageTitle"
+      class="header"
+      theme="paleblue"
+      data-test="blog-page-title"
+    >
+      <template v-if="showPageSummary">
+        <RichText :rich-text-content="pageSummary" />
+      </template>
+    </SectionWrapper>
 
-        <divider-general />
-      </div>-->
+    <SectionWrapper
+      class="dividers"
+      theme="paleblue"
+    >
+      <DividerWayFinder />
+    </SectionWrapper>
 
-      <h3>Featured Articles</h3>
-      <pre>{{ featuredArticles }}</pre>
+    <SectionWrapper
+      class="blog-section-title"
+      theme="paleblue"
+      :level="3"
+    >
+      <SectionHeader :level="3">
+        {{ isMobile ? 'Featured Blog' : 'Featured Blogs' }}
+      </SectionHeader>
 
-      <divider-general />
+      <div class="featured-articles-wrapper">
+        <BlockCardWithImage
+          v-for="article, index in parsedFeaturedArticles"
+          :key="article.title"
+          :image="article.image"
+          :to="article.to"
+          :date-created="article.dateCreated"
+          :category="article.category"
+          class="featured-article"
+          :data-test="`featured-blog-${index}`"
+        >
+          <template #title>
+            <RichText :rich-text-content="article.title" />
+          </template>
+          <template #description>
+            <RichText :rich-text-content="article.text" />
+          </template>
+        </BlockCardWithImage>
+      </div>
+    </SectionWrapper>
 
-      <h3>Article List (Truncated)</h3>
-      <pre> {{ articleList.slice(0, 15) }}</pre>
-    </section-wrapper>
+    <SectionWrapper
+      class="dividers"
+      theme="paleblue"
+    >
+      <DividerWayFinder />
+    </SectionWrapper>
+
+    <SectionWrapper
+      class="blog-section-title"
+      theme="paleblue"
+      :level="3"
+    >
+      <SectionHeader :level="3">
+        Latest Blogs
+      </SectionHeader>
+
+      <div class="articles-list-wrapper">
+        <SectionStaffArticleList
+          :items="parsedArticles"
+          data-test="latest-blogs"
+        />
+
+        <SectionPagination
+          v-if="totalPages !== 1 && !isMobile"
+          :pages="totalPages"
+          :initial-current-page="currentPage"
+        />
+      </div>
+    </SectionWrapper>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 @import 'assets/styles/listing-pages.scss';
+
+.page-article-list {
+  position: relative;
+  background-color: var(--pale-blue);
+
+  .header {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: center;
+    align-items: center;
+    text-align: center;
+    max-width: 787px;
+  }
+
+  .header>:deep(.section-header) {
+    margin-top: 20px;
+    margin-bottom: 0px;
+
+    h2.section-title {
+      color: $heading-grey;
+    }
+
+  }
+
+  .dividers {
+    padding-block: 0;
+
+    .divider-way-finder {
+      margin-block: 0;
+    }
+  }
+
+  .featured-articles-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr auto;
+    row-gap: 2.5rem;
+    column-gap: 1.75rem;
+  }
+
+  .featured-article {
+    background-color: var(--color-white);
+  }
+
+  .featured-article:nth-of-type(1) {
+    grid-column: 1 / 3;
+    grid-row: 1;
+  }
+
+  .featured-article:nth-of-type(2) {
+    grid-column: 1 / 2;
+  }
+
+  .featured-article:nth-of-type(3) {
+    grid-column: 2 / 3;
+  }
+
+  .featured-article:nth-of-type(2),
+  .featured-article:nth-of-type(3) {
+    grid-row: 2;
+  }
+
+  :deep(.custom-title),
+  :deep(.custom-description) {
+    .rich-text {
+      padding-right: 0;
+    }
+
+    .parsed-content {
+      margin-bottom: 0;
+    }
+  }
+
+  .blog-section-title {
+    :deep(.section-header) {
+      font-size: 38px;
+      margin-bottom: 48px;
+      color: $heading-grey;
+    }
+  }
+
+  .articles-list-wrapper {
+    background-color: var(--color-white);
+    margin-bottom: 20px;
+  }
+
+  .ftva.section-pagination {
+    margin-inline: auto;
+    padding: 2.5%;
+  }
+
+  @media #{$small} {
+    .featured-articles-wrapper {
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr;
+      row-gap: unset;
+      column-gap: unset;
+    }
+
+    .featured-article:nth-of-type(2),
+    .featured-article:nth-of-type(3) {
+      display: none;
+    }
+
+    .blog-section-title {
+      :deep(.section-header) {
+        font-size: 28px;
+        margin-bottom: 16px;
+      }
+    }
+  }
+}
 </style>
