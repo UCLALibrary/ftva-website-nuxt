cda97d05324cab570084724c586aacd56affedbb
jendiamond <thejendiamond@gmail.com>
2025-01-31 14:31:58 -0800

    comment out blogArticlePage.cy because of Craft errors

--- cypress/e2e/blogArticlePage.cy.js
+++ cypress/e2e/blogArticlePage.cy.js
@@ -1,8 +1,8 @@
-// describe('Blog Article Page', () => {
-//   it('Visits a Blog Article Page', () => {
-//     cy.visit('/blog/test-tom-reeds-for-members-only-black-perspectives-on-local-l-a-tv')
-//     cy.getByData('breadcrumb').should('be.visible')
-//     cy.getByData('recent-posts').should('be.visible')
-//     cy.percySnapshot('blogarticlepage', { widths: [768, 992, 1200] })
-//   })
-// })
+describe('Blog Article Page', () => {
+  it('Visits a Blog Article Page', () => {
+    cy.visit('/blog/test-tom-reeds-for-members-only-black-perspectives-on-local-l-a-tv')
+    cy.getByData('breadcrumb').should('be.visible')
+    cy.getByData('recent-posts').should('be.visible')
+    cy.percySnapshot('blogarticlepage', { widths: [768, 992, 1200] })
+  })
+})
