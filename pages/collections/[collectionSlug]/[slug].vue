212616f95635c40f90d5a102feca8a82606d7a32
tinuola <tinuola@users.noreply.github.com>
2025-02-19 14:09:51 -0800

    wip

--- pages/collections/[collectionSlug]/[slug].vue
+++ pages/collections/[collectionSlug]/[slug].vue
@@ -1,4 +1,6 @@
 <script setup>
+import { CardMeta, DividerWayFinder, NavBreadcrumb, ResponsiveImage, ResponsiveVideo, SectionWrapper, TwoColLayoutWStickySideBar, VideoEmbed } from 'ucla-library-website-components'
+
 // HELPERS
 import _get from 'lodash/get'
 
@@ -60,6 +62,47 @@ watch(data, (newVal, oldVal) => {
   relatedContent.value = _get(newVal, 'entries', {})
 })
 
+const parsedMainContent = computed(() => {
+
+})
+
+const parsedMediaContent = computed(() => {
+  if (!page.videoEmbed) {
+    return null
+  }
+
+  // return '<figure><iframe width="560" height="315" src="https://www.youtube.com/embed/uYr_SvIKKuI?si=ihenbmyE91KqyXK5" title="YouTube video player" frameborder="0"></iframe></figure>'
+})
+
+const parsedCredits = computed(() => {
+  if (page.value.associatedIndividuals.length === 0) {
+    return null
+  }
+
+  const credits = page.value.associatedIndividuals.map((obj) => {
+    const firstname = obj.individual[0].nameFirst
+    const lastname = obj.individual[0].nameLast
+    let fullname
+
+    if (!lastname) {
+      fullname = firstname
+    } else if (!firstname) {
+      fullname = lastname
+    } else {
+      fullname = `${firstname} ${lastname}`
+    }
+
+    return {
+      name: fullname,
+      role: obj.roles,
+    }
+  })
+
+  return credits
+})
+
+console.log('Parsed Credits: ', parsedCredits.value)
+
 // Entries query returns 4 random articles; main article might be included in the randomized return; to prevent duplication, filter out the main article; use remaining content in the related section.
 const parsedRelatedContent = computed(() => {
   const mainContentId = page.value.id
@@ -84,10 +127,74 @@ useHead({
 
 <template>
   <main
-    class="page page-collection-item-detail"
-    style="padding: 25px 100px;"
+    id="main"
+    class="page page-detail page-detail--white page-collection-item-detail"
   >
-    <section-wrapper>
+    <div class="header">
+      <NavBreadcrumb
+        class="breadcrumb"
+        :title="page?.title"
+      />
+    </div>
+
+    <TwoColLayoutWStickySideBar
+      data-test=""
+      class="two-column"
+    >
+      <template #primaryTop>
+        <CardMeta
+          :title="page?.title"
+          category="LA Rebellion"
+        >
+          <!-- <template #linkedcategoryslot>
+            <NuxtLink :to="">
+              {{ page?.title }}
+            </NuxtLink>
+          </template> -->
+        </CardMeta>
+      </template>
+
+      <template #primaryMid>
+        <ResponsiveVideo
+          v-if="parsedMediaContent"
+          :aspect-ratio="56.9"
+          :controls="true"
+        >
+          <template #default>
+            <VideoEmbed
+              :trailer="parsedMediaContent"
+              :poster-image="page.ftvaImage[0]"
+            />
+          </template>
+        </ResponsiveVideo>
+        <ResponsiveImage
+          v-else
+          :media="page.ftvaImage[0]"
+        />
+        <h2
+          v-if="page?.richText"
+          class=""
+        >
+          Sypnosis
+        </h2>
+        <RichText
+          v-if="page?.richText"
+          class="eventDescription"
+          :rich-text-content="page?.richText"
+        />
+      </template>
+
+      <template
+        v-if="parsedCredits"
+        #primaryBottom
+      >
+        <DividerWayFinder />
+        <pre>{{ parsedCredits }}</pre>
+      </template>
+    </TwoColLayoutWStickySideBar>
+
+    <SectionWrapper />
+    <!-- <section-wrapper>
       <h2>Collection Item</h2>
       <pre>{{ page }}</pre>
       <divider-general />
@@ -104,8 +211,19 @@ useHead({
         <p>image: {{ item.ftvaImage[0] }}</p>
         <divider-general />
       </div>
-    </section-wrapper>
+    </section-wrapper> -->
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page-collection-item-detail {
+  position: relative;
+
+  .header {
+    background-color: var(--pale-blue);
+    padding-block: 0.5rem;
+  }
+}
+
+@import 'assets/styles/slug-pages.scss';
+</style>
