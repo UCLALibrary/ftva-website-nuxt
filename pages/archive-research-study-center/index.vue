6091f71ba1866b56034ed92e2483293e104b36c0
Jess <jessica.divers@gmail.com>
2025-03-12 16:47:03 -0700

    feat: APPS-3244 new route and GQL (#121)
    * feat: new route and gql
    
    * chore: cleanup commented code
    
    * fix: page specific data name
    
    * feat: add  missing route
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>
    Co-authored-by: pghorpade <pghorpade@library.ucla.edu>

---
+++ pages/archive-research-study-center/index.vue
@@ -0,0 +1,89 @@
+<script setup>
+// COMPONENTS
+import { SectionWrapper, SectionHeader, SectionStaffArticleList, RichText, DividerWayFinder, BlockCardWithImage } from '@ucla-library-monorepo/ucla-library-website-components'
+
+// HELPERS
+import _get from 'lodash/get'
+
+// GQL
+import FTVAARSCIMCS from '../gql/queries/FTVAEntryARSCIMCS.gql'
+
+const { $graphql } = useNuxtApp()
+
+const route = useRoute()
+// routes this page supports:
+const routeNameToSectionMap = {
+  '/archive-research-study-center': 'ftvaArchiveResearchAndStudyCenter',
+  '/instructional-media-collections-services': 'ftvaInstructionalMediaCollectionsAndServices'
+}
+const { data, error } = await useAsyncData(route.path, async () => {
+  // lookup section based on routeNameToSectionMap
+  const data = await $graphql.default.request(FTVAARSCIMCS, { section: routeNameToSectionMap[route.path] })
+  return data
+})
+
+if (error.value) {
+  throw createError({
+    ...error.value, statusMessage: 'Page not found.' + error.value, fatal: true
+  })
+}
+
+if (!data.value.entry) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found',
+    fatal: true
+  })
+}
+
+// DATA
+const page = ref(_get(data.value, 'entry', {}))
+
+// PREVIEW WATCHER FOR CRAFT CONTENT
+watch(data, (newVal, oldVal) => {
+  page.value = _get(newVal, 'entry', {})
+})
+
+// PAGE SUMMARY
+const showPageSummary = computed(() => {
+  return page.value?.summary && page.value?.displaySummary === 'yes'
+})
+
+definePageMeta({
+  layout: 'default',
+  path: '/archive-research-study-center',
+  alias: ['/instructional-media-collections-services']
+})
+
+useHead({
+  title: page.value ? page.value.title : '... loading',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: removeTags(page.value.summary)
+    }
+  ]
+})
+</script>
+
+<template>
+  <!-- todo write in page route.name dynamically in class? need some specific class -->
+  <main
+    id="main"
+    class="page"
+  >
+    <SectionWrapper
+      :section-title="page.title"
+      class="header"
+      data-test=""
+    >
+      <template v-if="showPageSummary">
+        <RichText :rich-text-content="page.summary" />
+      </template>
+    </SectionWrapper>
+    <SectionWrapper>
+      {{ page }}
+    </SectionWrapper>
+  </main>
+</template>
