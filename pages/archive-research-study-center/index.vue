cc833a6a0e1bb6aec94ef2d29476f0ed2079229c
Jess Divers <jessica.divers@library.ucla.edu>
2025-03-25 15:38:58 -0700

    fix: page styles

--- pages/archive-research-study-center/index.vue
+++ pages/archive-research-study-center/index.vue
@@ -46,6 +46,86 @@ const showPageSummary = computed(() => {
   return page.value?.summary && page.value?.displaySummary === 'yes'
 })
 
+const pageClass = computed(() => {
+  return ['page', 'page-detail', 'page-detail--paleblue', route.name]
+})
+
+// TODO MOVE TO GLOBAL UTILS, find and remove others
+// Get data for Image or Carousel at top of page
+// TODO WHAT ABOUT ftvaimage = map to image?
+const parsedImage = computed(() => {
+  return page.value.imageCarousel
+  // todo add logic to check for image field if no carousel?
+})
+
+// Transform data for Carousel
+const parsedCarouselData = computed(() => {
+  // map image to item, map creditText to credit
+  return parsedImage.value.map((rawItem, index) => {
+    return {
+      item: [{ ...rawItem.image[0], kind: 'image' }], // Carousels on this page are always images, no videos
+      credit: rawItem?.creditText,
+    }
+  })
+})
+// END TODO
+
+// TODO MOVE TO A UTIL
+// infoblocks reform
+const parsedFlexibleBlocks = computed(() => {
+  return page.value.blocks.map((block) => {
+    // check for infoblock
+    if (block.typeHandle !== 'infoBlock') {
+      return block
+    }
+
+    //if found reformat
+    // const reformat = ref({
+    //   ...block,
+    //   infoBlock: [{
+    //     ...block.infoBlock[0],
+    //     id: block.id
+    //   }]
+    // })
+    // console.log('reformat', reformat)
+    return {
+      ...block,
+      infoBlock: [{
+        ...block.infoBlock[0],
+        id: block.id
+      }]
+    }
+  })
+})
+
+// item in data currently
+const item = {
+  id: "3761992",
+  typeHandle: "infoBlock",
+  infoBlock: [{
+    typeHandle: "contactInfoBlock",
+    email: "arsc@cinema.ucla.edu",
+    phone: "+1 310-206-5388",
+    address: "<p translate=\"no\">\n<span class=\"address-line1\">University of California, Los Angeles</span><br>\n<span class=\"address-line2\">Powell Library (Room 46)</span><br>\n<span class=\"locality\">Los Angeles</span>, <span class=\"administrative-area\">CA</span> <span class=\"postal-code\">90095-1517</span><br>\n<span class=\"country\">United States</span>\n</p>"
+  }]
+}
+
+const mockContactInfoBlock = {
+  id: '3726206',
+  typeHandle: 'infoBlock',
+  infoBlock: [
+    {
+      typeHandle: 'contactInfoBlock',
+      id: '3726207',
+      email: 'archive@email.com',
+      phone: '+1 323-555-1234',
+      address: '<p translate="no">\n<span class="address-line1">722 California Ave</span><br>\n<span class="locality">Glendale</span>, <span class="administrative-area">CA</span> <span class="postal-code">90210</span><br>\n<span class="country">United States</span>\n</p>'
+    }
+  ]
+}
+//
+// { "id": "3761987", "typeHandle": "infoBlock", "infoBlock": [{ "typeHandle": "infoBlock", "icon": "icon-ftva-download", "heading": "MONDAY - FRIDAY, 9 A.M. TO 5 P.M.", "text": "<p>Hours are subject to change. Requests to view materials must be arranged in advance.</p>" }] }
+
 definePageMeta({
   layout: 'default',
   path: '/archive-research-study-center',
@@ -65,11 +145,43 @@ useHead({
 </script>
 
 <template>
-  <!-- todo write in page route.name dynamically in class? need some specific class -->
   <main
     id="main"
-    class="page"
+    :class="pageClass"
   >
+    <div class="one-column">
+      <ResponsiveImage
+        v-if="parsedImage && parsedImage.length === 1 && parsedImage[0]?.image && parsedImage[0]?.image?.length === 1"
+        data-test="single-image"
+        :media="parsedImage[0]?.image[0]"
+        :aspect-ratio="43.103"
+      >
+        <template
+          v-if="parsedImage[0]?.creditText"
+          #credit
+        >
+          {{ parsedImage[0]?.creditText }}
+        </template>
+      </ResponsiveImage>
+      <div
+        v-else-if="parsedCarouselData && parsedCarouselData.length > 0 && parsedImage[0].image.length !== 0"
+        class="lightbox-container"
+      >
+        <FlexibleMediaGalleryNewLightbox
+          data-test="image-carousel"
+          :items="parsedCarouselData"
+          inline="true"
+        >
+          <template #default="slotProps">
+            <BlockTag
+              data-test="credit-text"
+              :label="parsedCarouselData[slotProps.selectionIndex]?.creditText"
+            />
+          </template>
+        </FlexibleMediaGalleryNewLightbox>
+      </div>
+      <!-- TODO END -->
+    </div>
     <SectionWrapper
       :section-title="page.title"
       class="header"
@@ -79,8 +191,23 @@ useHead({
         <RichText :rich-text-content="page.summary" />
       </template>
     </SectionWrapper>
-    <SectionWrapper>
-      {{ page }}
-    </SectionWrapper>
+
+    <FlexibleBlocks
+      class="flexible-content"
+      :blocks="page.blocks"
+    />
+    <!-- {{ parsedFlexibleBlocks }} -->
   </main>
 </template>
+<style lang="scss" scoped>
+@import 'assets/styles/slug-pages.scss';
+
+.one-column {
+
+  // if the layout has an image or carousel at the top
+  &:has(> .lightbox-container),
+  &:has(> figure) {
+    padding-top: 80px; // to account for the missing pageanchor on this layout
+  }
+}
+</style>
