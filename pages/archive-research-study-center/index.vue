38ad132638c477d4ebf4f28e0719491cb96c4dcd
Jess <jessica.divers@gmail.com>
2025-03-26 16:48:51 -0700

    feat: APPS-3246 finish assembling arsc & IMCS page (#127)
    * fix: page styles
    
    * chore: update package, cleanup comments, lint
    
    * fix: condition
    
    * feat: add cypress tests
    
    * fix: rerun tests
    
    * fix: remove extra styles
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>
    Co-authored-by: Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>

--- pages/archive-research-study-center/index.vue
+++ pages/archive-research-study-center/index.vue
@@ -46,6 +46,24 @@ const showPageSummary = computed(() => {
   return page.value?.summary && page.value?.displaySummary === 'yes'
 })
 
+const pageClass = computed(() => {
+  return ['page', 'page-detail', 'page-detail--paleblue', route.path.slice(1)]
+})
+
+const parsedImage = computed(() => {
+  return page.value.imageCarousel
+})
+
+const parsedCarouselData = computed(() => {
+  // map image to item, map creditText to credit
+  return parsedImage.value.map((rawItem, index) => {
+    return {
+      item: [{ ...rawItem.image[0], kind: 'image' }], // Carousels on this page are always images, no videos
+      credit: rawItem?.creditText,
+    }
+  })
+})
+
 definePageMeta({
   layout: 'default',
   path: '/archive-research-study-center',
@@ -63,13 +81,43 @@ useHead({
   ]
 })
 </script>
-
 <template>
-  <!-- todo write in page route.name dynamically in class? need some specific class -->
   <main
     id="main"
-    class="page"
+    :class="pageClass"
   >
+    <div class="one-column">
+      <ResponsiveImage
+        v-if="parsedImage && parsedImage.length === 1 && parsedImage[0]?.image && parsedImage[0]?.image?.length === 1"
+        data-test="single-image"
+        :media="parsedImage[0]?.image[0]"
+        :aspect-ratio="43.103"
+      >
+        <template
+          v-if="parsedImage[0]?.creditText"
+          #credit
+        >
+          {{ parsedImage[0]?.creditText }}
+        </template>
+      </ResponsiveImage>
+      <div
+        v-else-if="parsedCarouselData && parsedCarouselData.length > 0 && parsedImage[0]?.image && parsedImage[0].image.length !== 0"
+        class="lightbox-container"
+      >
+        <FlexibleMediaGalleryNewLightbox
+          data-test="image-carousel"
+          :items="parsedCarouselData"
+          inline="true"
+        >
+          <template #default="slotProps">
+            <BlockTag
+              data-test="credit-text"
+              :label="parsedCarouselData[slotProps.selectionIndex]?.creditText"
+            />
+          </template>
+        </FlexibleMediaGalleryNewLightbox>
+      </div>
+    </div>
     <SectionWrapper
       :section-title="page.title"
       class="header"
@@ -79,8 +127,22 @@ useHead({
         <RichText :rich-text-content="page.summary" />
       </template>
     </SectionWrapper>
-    <SectionWrapper>
-      {{ page }}
-    </SectionWrapper>
+
+    <FlexibleBlocks
+      class="flexible-content"
+      :blocks="page.blocks"
+    />
   </main>
 </template>
+<style lang="scss" scoped>
+@import 'assets/styles/slug-pages.scss';
+
+.one-column {
+
+  // if the layout has an image or carousel at the top
+  &:has(> .lightbox-container),
+  &:has(> figure) {
+    padding-top: 80px; // to account for the missing pageanchor on this layout
+  }
+}
+</style>
