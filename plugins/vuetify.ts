49488b0044c0c35f2f150db3746c37042f3bd642
tinuola <tinuola@users.noreply.github.com>
2025-03-27 14:24:31 -0700

    delete vuetify styles, mdi imports from vuetify plugin

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,9 +1,7 @@
 // plugins/vuetify.ts
-import 'vuetify/styles' // Import Vuetify styles
 import { createVuetify } from 'vuetify'
 import { VCalendar } from 'vuetify/labs/VCalendar' // Import VCalendar
 import { VCard, VList, VListItem, VMenu, VSheet } from 'vuetify/lib/components/index.mjs'
-import '@mdi/font/css/materialdesignicons.css' // Import Material Design Icons
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
