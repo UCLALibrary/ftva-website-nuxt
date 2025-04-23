e881ffa1c26efac6e06c04ade363eb03ba9f5741
tinuola <tinuola@users.noreply.github.com>
2025-04-23 12:46:10 -0700

    Add parsed collection list

--- pages/collections/motion-picture/index.vue
+++ pages/collections/motion-picture/index.vue
@@ -49,6 +49,17 @@ if (!data.value.entry) {
 // DATA
 const page = ref(_get(data.value, 'entry', {}))
 console.log('Page data: ', page.value)
+const pageTitle = page.value.title
+const pageSummary = page.value.summary
+const generalContentPages = page.value.associatedGeneralContentPagesFtva
+
+// PREVIEW WATCHER FOR CRAFT CONTENT
+watch(data, (newVal, oldVal) => {
+  page.value = _get(newVal, 'entry', {})
+  pageTitle.value = page.value.title
+  pageSummary.value = page.value.summary
+  generalContentPages.value = page.value.associatedGeneralContentPagesFtva
+})
 
 // TESTING ES
 const collectionType = ref(routeNameToSectionMap[route.path].collection)
@@ -74,6 +85,7 @@ async function testES() {
 
   console.log('Browse-by: ', extraFilters.value)
   collectionList.value = esOutput.hits.hits
+  console.log('Collection List: ', collectionList.value)
   totalDocuments.value = esOutput.hits.total.value
 }
 
@@ -82,9 +94,19 @@ watch(alphabet, (newVal, oldVal) => {
   testES()
 })
 
-// PREVIEW WATCHER FOR CRAFT CONTENT
-watch(data, (newVal, oldVal) => {
-  page.value = _get(newVal, 'entry', {})
+const parsedCollectionList = computed(() => {
+  if (collectionList.value.length === 0) return []
+
+  return collectionList.value.map((obj) => {
+    return {
+      ...obj.source,
+      to: `/${obj._source.uri}`,
+      title: obj._source.title,
+      text: obj._source.richText.replace(/<img.*?>/ig, ''),
+      ftvaCollectionType: obj._source.ftvaCollectionType,
+      image: parseImage(obj)
+    }
+  })
 })
 
 definePageMeta({
@@ -113,7 +135,7 @@ useHead({
     <SectionWrapper theme="paleblue">
       <NavBreadcrumb
         data-test="breadcrumb"
-        :title="page?.title"
+        :title="pageTitle"
       />
     </SectionWrapper>
 
@@ -123,7 +145,7 @@ useHead({
       theme="paleblue"
       data-test="page-title"
     >
-      <RichText :rich-text-content="page.summary" />
+      <RichText :rich-text-content="pageSummary" />
     </SectionWrapper>
 
     <SectionWrapper theme="paleblue">
@@ -139,7 +161,7 @@ useHead({
       />
 
       <div class="collection-type-list-wrapper">
-        <SectionTeaserCard class="collection-type-cards" />
+        <SectionTeaserCard :items="parsedCollectionList" />
 
         <SectionPagination
           v-if="totalPages !== 1 && !isMobile"
@@ -182,5 +204,36 @@ useHead({
     text-align: center;
     max-width: 787px;
   }
+
+  :deep(.block-highlight) {
+    border-radius: 0;
+
+    .media,
+    &.is-vertical .image-container .molecule-no-image {
+      border-radius: 0;
+    }
+
+    &.is-vertical.card:hover {
+      box-shadow: none;
+    }
+
+    .card-meta.card-meta-items:hover>a.title {
+      text-decoration: underline;
+      text-decoration-color: #2C91FF;
+      text-decoration-thickness: 3px;
+      text-underline-offset: 3px;
+    }
+  }
+
+  :deep(.card-meta) {
+    background-color: var(--pale-blue);
+  }
+
+  // @media(min-width: 991px) {
+  //   .ftva.block-highlight.is-vertical.card {
+  //     height: 550px;
+  //   }
+  // }
+
 }
 </style>
