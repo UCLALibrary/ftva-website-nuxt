e7f7e7e790e3d1ceb03aa3a23c66273fbafad711
tinuola <tinuola@users.noreply.github.com>
2025-05-20 14:23:27 -0700

    Change item used in CollectionItemDetail cypress test

--- cypress/e2e/collectionItemDetailPage.cy.js
+++ cypress/e2e/collectionItemDetailPage.cy.js
@@ -1,6 +1,6 @@
 describe('Collection Item Detail Page', () => {
   it('Visits a Collection Item Detail Page', () => {
-    cy.visit('/collections/la-rebellion/grey-area')
+    cy.visit('/collections/ktla-newsfilm-collection/african-american-william-c-taylor-of-the-communist-party-to-run-for-los-angeles-county-supervisor')
     cy.getByData('breadcrumb').should('be.visible')
     cy.getByData('page-title').should('be.visible')
     cy.getByData('metadata').should('be.visible')
