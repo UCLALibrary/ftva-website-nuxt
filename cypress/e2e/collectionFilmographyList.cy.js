c0eec5e5950c00d697f98d8a083a58fa4f077509
Jess Divers <jessica.divers@library.ucla.edu>
2025-05-29 16:37:51 -0700

    fix: tests

--- cypress/e2e/collectionFilmographyList.cy.js
+++ cypress/e2e/collectionFilmographyList.cy.js
@@ -2,12 +2,12 @@ describe('Collection Filmography List Page', () => {
   it('Visits the `L.A. Rebellion` Filmography', () => {
     cy.visit('/collections/la-rebellion/filmography')
     cy.getByData('breadcrumb').should('be.visible')
-    cy.getByClass('page-collections-list-of-items').should('be.visible')
+    cy.get('page-collections-list-of-items').should('be.visible')
   })
 
   it('Visits the `In The Life` Filmography', () => {
     cy.visit('/collections/in-the-life/filmography')
     cy.getByData('breadcrumb').should('be.visible')
-    cy.getByClass('page-collections-list-of-items').should('be.visible')
+    cy.get('page-collections-list-of-items').should('be.visible')
   })
 })
