d0cf08fd64846a90df9a53afb61397aab3f7861d
tinuola <tinuola@users.noreply.github.com>
2025-06-10 15:13:09 -0700

    Comment out image fraqments in query

--- pages/collections/index.vue
+++ pages/collections/index.vue
@@ -18,7 +18,7 @@ if (error.value) {
   })
 }
 
-if (!data.value.entries) {
+if (!data.value.entry) {
   // console.log('no data')
   throw createError({
     statusCode: 404,
@@ -26,42 +26,59 @@ if (!data.value.entries) {
     fatal: true
   })
 }
-// TODO This is creating an index of the content for ES search
 
-const page = ref(_get(data.value, 'entries[0]', {}))
+// METADATA INFO
+if (data.value.entry && import.meta.prerender) {
+  try {
+    // Call the composable to use the indexing function
+    const { indexContent } = useContentIndexer()
+    const doc = {
+      title: data.value.entry.title,
+      text: data.value.entry.summary,
+      uri: '/collections',
+      sectionHandle: data.value.entry.sectionHandle,
+      groupName: 'Collections',
+    }
+    // Index the articles data using the composable during static build
+    await indexContent(doc, 'explore-collections')
+    // console.log('Explore Collections content indexed successfully during static build')
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('FAILED TO EXPLORE COLLECTIONS LISTING during static build:', error)
+  }
+}
+
+// DATA
+const page = ref(_get(data.value, 'entry', {}))
+console.log('page data: ', page.value)
+
+// PREVIEW WATCHER FOR CRAFT CONTENT
+watch(data, (newVal, oldVal) => {
+  page.value = _get(newVal, 'entry', {})
+})
+
 useHead({
-  title: page.value?.title || '... loading',
+  title: page.value ? page.value.title : '... loading',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: removeTags(page.value.summary)
+    }
+  ]
 })
 </script>
 
 <template>
-  <div
-    class="page page-collections"
-    style="padding: 25px 100px;"
+  <main
+    id="main"
+    class="page"
   >
-    <section-wrapper section-title="Collections">
-      <h2>SEACHBAR</h2>
-
-      <div
-        v-for="collection in data.entries"
-        :key="collection?.id"
-      >
-        <NuxtLink :to="`/${collection?.to}`">
-          {{ collection?.title }}
-        </NuxtLink> <br>
-        <h4>sectionHandle: <code>{{ collection?.sectionHandle }}</code></h4>
-        <h4>ftvaCollectionType: <code>{{ collection?.ftvaCollectionType }}</code></h4>
-        <h4>richText: <code>{{ collection?.richText }}</code></h4>
-        <h4>videoEmbed: <code>{{ collection?.videoEmbed }}</code></h4>
-        <h4>viewAllSectionLink: <code>{{ collection?.viewAllSectionLink }}</code></h4>
-        <h4>infoBlock: <code>{{ collection?.infoBlock }}</code></h4>
-        <h4>image: <code>{{ collection?.image }}</code></h4>
-        <divider-general />
-      </div>
-
-      <!-- <code><strong>PAGE</strong> {{ page }}</code> -->
-    </section-wrapper>
-  </div>
+    <SectionWrapper>
+      <h1>{{ page.title }}</h1>
+      <pre style="text-wrap: auto;">{{ page }}</pre>
+    </SectionWrapper>
+  </main>
 </template>
 
 <style scoped>
