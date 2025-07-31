53d0ad79aab23b27ac365f516762c5180e6f80cd
Jess Divers <jessica.divers@library.ucla.edu>
2025-07-31 13:44:07 -0700

    chore: run linter

--- composables/useMobileOnlyInfiniteScroll.ts
+++ composables/useMobileOnlyInfiniteScroll.ts
@@ -70,7 +70,7 @@ export default function useMobileInfiniteScroll<T = any>(
         await searchES()
       }
     },
-    { 
+    {
       distance: 100,
       canLoadMore: () => {
         // Only allow infinite scroll on mobile and when there's more content
