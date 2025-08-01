3609737ce0f44e4ac2025eb2ef9d52e40fccbed8
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2025-08-01 10:50:52 -0700

    feat: APPS-3096 Assemble Filmmakers Listing page (#185)
    * wip: assemble page components
    
    * Tiny code updates to blog slug, blog and series indices
    
    * Add one-column class to adjust motionPicture breadcrumb
    
    * Remove excess space above blog listing title
    
    * Fix spacing between header and lists on medium screen
    
    * Minor adjustment to align breadcrumbs similar to other pages
    
    * Logic to show/hide filmmaker description on mobile
    
    * Add basic Cypress test
    
    * fix: use correct key for listings url
    
    * Update Nuxt modules
    
    * Update design tokens
    
    * Fix typo in cypress url
    
    * Styling to unstack/adjust listings on mobile
    
    * Implement sort functionality
    
    * UX requested styling fixes
    
    * fix selector typo
    
    * style: listing cards fixes
    
    * style: fixes
    
    ---------
    
    Co-authored-by: tinuola <tinuola@users.noreply.github.com>

---
+++ cypress/e2e/collectionFilmmakersList.cy.js
@@ -0,0 +1,10 @@
+// Currently exists only for LA Rebellion
+describe('Collection Filmmakers Listing Page', () => {
+  it('Visits the `L.A. Rebellion` Filmmakers Listing page', () => {
+    cy.visit('/collections/la-rebellion/filmmakers')
+    cy.getByData('breadcrumb').should('be.visible')
+    cy.getByData('page-heading').should('be.visible')
+    cy.getByData('page-listings').should('be.visible')
+    cy.percySnapshot('collectionFilmmakersListPage', { widths: [768, 992, 1200] })
+  })
+})
