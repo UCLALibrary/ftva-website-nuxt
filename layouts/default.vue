717d132315b3a4c27298bf7f8d5bae3059c25c56
tinuola <tinuola@users.noreply.github.com>
2025-08-05 09:51:20 -0700

    Revert changes to default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -50,7 +50,7 @@ onMounted(async () => {
 <template lang="html">
   <div :class="classes">
     <!-- site brand bar only shows on desktop -->
-    <site-brand-bar />
+    <site-brand-bar class="brand-bar" />
     <header-sticky
       v-if="primaryMenuItems"
       class="primary"
@@ -81,9 +81,8 @@ onMounted(async () => {
   flex: 1 1 auto;
 
   .brand-bar {
-    // display: none;
-    // width: 100%;
-    // z-index: 100;
+    width: 100%;
+    z-index: 100;
   }
 
   .primary {
@@ -92,18 +91,8 @@ onMounted(async () => {
   }
 
   @media #{$small} {
-    // .brand-bar {
-    // display: none;
-    // }
-
-    :deep(.nav-background-fill) {
-      background-color: red !important;
-    }
-
-    // :deep(.nav-primary .more-menu > .close-button) {
-    .header-sticky .close-button {
-      right: 50px;
-      position: static;
+    .brand-bar {
+      display: none;
     }
   }
 }
