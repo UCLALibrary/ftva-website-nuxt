48a4429c2cc31503389ace2367ee5cc78f6099c8
Jess Divers <jessica.divers@library.ucla.edu>
2025-09-04 15:13:16 -0700

    fix: failed to resolve component errors

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,12 @@
 // plugins/vuetify.ts
 import { createVuetify } from 'vuetify'
 import { VCalendar } from 'vuetify/labs/VCalendar' // Import VCalendar
-import { VCard, VList, VListItem, VMenu, VSheet, VSlideGroup, VSlideGroupItem } from 'vuetify/lib/components/index.mjs'
+import { VCard, VList, VListItem, VMenu, VSheet, VSlideGroup, VSlideGroupItem, VWindow, VWindowItem } from 'vuetify/lib/components/index.mjs'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components: {
-      VCalendar, VSheet, VMenu, VList, VCard, VListItem, VSlideGroup, VSlideGroupItem,
+      VCalendar, VSheet, VMenu, VList, VCard, VListItem, VSlideGroup, VSlideGroupItem, VWindow, VWindowItem
     },
   })
 
