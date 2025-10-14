616d6373fe133449ff4d8ff5beae5afb67dee24b
pghorpade <pghorpade@library.ucla.edu>
2025-10-14 14:12:03 -0700

    fis: cypress tests

--- cypress/e2e/generalContent.cy.js
+++ cypress/e2e/generalContent.cy.js
@@ -2,6 +2,6 @@ describe('General Content page', () => {
   it('Visit a General Content Page', () => {
     cy.visit('/about')
 
-    cy.percySnapshot({ widths: [768, 992, 1200] })
+    cy.percySnapshot('generalContentPage',{ widths: [768, 992, 1200] })
   })
 })
