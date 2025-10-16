2dfc897bae9ed1f8d7cd74fdd5df71c952b343e0
tinuola <tinuola@users.noreply.github.com>
2025-10-16 14:59:20 -0700

    wip: set up touring listing composable

--- composables/useTouringSeriesListSearchFilter.js
+++ composables/useTouringSeriesListSearchFilter.js
@@ -1,5 +1,9 @@
 export default function useTouringSeriesListSearchFilter() {
-  return { pastTouringSeriesQuery, currentTouringSeriesQueryCurrent, currentTouringSeriesQueryOngoing }
+  return {
+    pastTouringSeriesQuery,
+    // currentTouringSeriesQueryCurrent, currentTouringSeriesQueryOngoing
+
+  }
 }
 
 async function pastTouringSeriesQuery(
@@ -34,7 +38,7 @@ async function pastTouringSeriesQuery(
             filter: [
               {
                 term: {
-                  'sectionHandle.keyword': 'ftvaListingTouringSeries'
+                  'sectionHandle.keyword': 'ftvaTouringSeries'
                 }
               },
               {
@@ -56,117 +60,117 @@ async function pastTouringSeriesQuery(
   return data
 }
 
-async function currentTouringSeriesQueryCurrent(
-  currentPage = 1,
-  documentsPerPage = 10,
-  source = ['*'],
-) {
-  const config = useRuntimeConfig()
-  if (
-    config.public.esReadKey === '' ||
-        config.public.esURL === '' ||
-        config.public.esAlias === ''
-  )
-    return
+// async function currentTouringSeriesQueryCurrent(
+//   currentPage = 1,
+//   documentsPerPage = 10,
+//   source = ['*'],
+// ) {
+//   const config = useRuntimeConfig()
+//   if (
+//     config.public.esReadKey === '' ||
+//         config.public.esURL === '' ||
+//         config.public.esAlias === ''
+//   )
+//     return
 
-  const response = await fetch(
-    `${config.public.esURL}/${config.public.esAlias}/_search`,
-    {
-      headers: {
-        Authorization: `ApiKey ${config.public.esReadKey}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        from: (currentPage - 1) * documentsPerPage,
-        size: documentsPerPage,
-        _source: [...source],
-        query: {
-          bool: {
-            filter: [
-              {
-                term: {
-                  'sectionHandle.keyword': 'ftvaListingTouringSeries',
-                },
-              },
-            ],
-            should: [
-              {
-                term: {
-                  ongoing: true,
-                },
-              },
-              {
-                range: {
-                  endDate: {
-                    gte: 'now/d-8h',
-                  },
-                },
-              },
-            ],
-            minimum_should_match: 1,
-          }
-        },
-        sort: [{ ongoing: { order: 'asc' } }, { startDate: { order: 'asc' } }],
-      }),
-    }
-  )
+//   const response = await fetch(
+//     `${config.public.esURL}/${config.public.esAlias}/_search`,
+//     {
+//       headers: {
+//         Authorization: `ApiKey ${config.public.esReadKey}`,
+//         'Content-Type': 'application/json',
+//       },
+//       method: 'POST',
+//       body: JSON.stringify({
+//         from: (currentPage - 1) * documentsPerPage,
+//         size: documentsPerPage,
+//         _source: [...source],
+//         query: {
+//           bool: {
+//             filter: [
+//               {
+//                 term: {
+//                   'sectionHandle.keyword': 'ftvaListingTouringSeries',
+//                 },
+//               },
+//             ],
+//             should: [
+//               {
+//                 term: {
+//                   ongoing: true,
+//                 },
+//               },
+//               {
+//                 range: {
+//                   endDate: {
+//                     gte: 'now/d-8h',
+//                   },
+//                 },
+//               },
+//             ],
+//             minimum_should_match: 1,
+//           }
+//         },
+//         sort: [{ ongoing: { order: 'asc' } }, { startDate: { order: 'asc' } }],
+//       }),
+//     }
+//   )
 
-  const data = await response.json()
-  return data
-}
+//   const data = await response.json()
+//   return data
+// }
 
-async function currentTouringSeriesQueryOngoing(
-  currentPage = 1,
-  documentsPerPage = 10,
-  sort,
-  orderBy,
-  source = ['*'],
-) {
-  const config = useRuntimeConfig()
-  if (
-    config.public.esReadKey === '' ||
-        config.public.esURL === '' ||
-        config.public.esAlias === ''
-  )
-    return
+// async function currentTouringSeriesQueryOngoing(
+//   currentPage = 1,
+//   documentsPerPage = 10,
+//   sort,
+//   orderBy,
+//   source = ['*'],
+// ) {
+//   const config = useRuntimeConfig()
+//   if (
+//     config.public.esReadKey === '' ||
+//         config.public.esURL === '' ||
+//         config.public.esAlias === ''
+//   )
+//     return
 
-  const response = await fetch(
-    `${config.public.esURL}/${config.public.esAlias}/_search`,
-    {
-      headers: {
-        Authorization: `ApiKey ${config.public.esReadKey}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        from: (currentPage - 1) * documentsPerPage,
-        size: documentsPerPage,
-        _source: [...source],
-        query: {
-          bool: {
-            filter: [
-              {
-                term: {
-                  'sectionHandle.keyword': 'ftvaListingTouringSeries',
-                },
-              },
-              {
-                term: {
-                  ongoing: true,
-                },
-              }
-            ]
-          }
-        },
-        ...parseSort(sort, orderBy),
-      }),
-    }
-  )
+//   const response = await fetch(
+//     `${config.public.esURL}/${config.public.esAlias}/_search`,
+//     {
+//       headers: {
+//         Authorization: `ApiKey ${config.public.esReadKey}`,
+//         'Content-Type': 'application/json',
+//       },
+//       method: 'POST',
+//       body: JSON.stringify({
+//         from: (currentPage - 1) * documentsPerPage,
+//         size: documentsPerPage,
+//         _source: [...source],
+//         query: {
+//           bool: {
+//             filter: [
+//               {
+//                 term: {
+//                   'sectionHandle.keyword': 'ftvaListingTouringSeries',
+//                 },
+//               },
+//               {
+//                 term: {
+//                   ongoing: true,
+//                 },
+//               }
+//             ]
+//           }
+//         },
+//         ...parseSort(sort, orderBy),
+//       }),
+//     }
+//   )
 
-  const data = await response.json()
-  return data
-}
+//   const data = await response.json()
+//   return data
+// }
 
 function parseSort(sortField, orderBy = 'asc') {
   if (!sortField || sortField === '') return {}
