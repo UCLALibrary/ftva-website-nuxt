6e20853945004a32c3069afed6397efc54fe3030
JenDiamond <thejendiamond@gmail.com>
2025-10-21 15:44:39 -0700

    fix: update the text on the event listing page (#253)
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -557,7 +557,7 @@ const pageClasses = computed(() => {
                 v-if="noResultsFound"
                 class="empty-tab"
               >
-                There are no upcoming events WITH THE FILTERS YOU SELECTED
+                There are no events matching the selected filters
               </p>
               <p
                 v-else
