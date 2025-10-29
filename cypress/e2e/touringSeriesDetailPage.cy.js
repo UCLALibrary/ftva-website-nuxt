f1eb306cb99729e375a5c6ae5e72f4155527108b
jendiamond <thejendiamond@gmail.com>
2025-10-28 17:00:30 -0700

    feat: remove the widths from the percy tests

--- cypress/e2e/touringSeriesDetailPage.cy.js
+++ cypress/e2e/touringSeriesDetailPage.cy.js
@@ -2,6 +2,6 @@ describe('Touring Series Detail page', () => {
   it('Visit the Touring Series detail page', () => {
     cy.visit('/touring-series/through-indian-eyes-native-american-cinema')
 
-    cy.percySnapshot('touringseriesdetailpage', { widths: [768, 992, 1200] })
+    cy.percySnapshot('touringseriesdetailpage')
   })
 })
