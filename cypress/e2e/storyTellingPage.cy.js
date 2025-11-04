3f3f1dd6cd379b9642ab77848efefb90df16ca3d
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2025-11-04 10:48:43 -0800

    fix: APPS-3544 Return spacing above IMCS banner/carousel (#272)
    * Return spacing above IMCS carousel
    
    * Update storytelling test variable for page image
    
    ---------
    
    Co-authored-by: tinuola <tinuola@users.noreply.github.com>
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>

--- cypress/e2e/storyTellingPage.cy.js
+++ cypress/e2e/storyTellingPage.cy.js
@@ -3,9 +3,13 @@ Cypress.on('uncaught:exception', () => { return false })
 describe('Complex Collection StoryTelling Page', () => {
   it('Visits the L.A. Rebellion StoryTelling Page', () => {
     cy.visit('/collections/la-rebellion')
-    cy.getByData('hero-image').should('be.visible')
+
+    cy.getByData('main-image').should('be.visible')
+
     cy.getByData('page-heading').should('be.visible')
+
     cy.getByData('flexible-blocks-content').should('be.visible')
+
     cy.percySnapshot('storyTellingPage')
   })
 })
