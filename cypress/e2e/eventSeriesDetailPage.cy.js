304bd22e975abbdf1f249e99c4b4b03981750a68
Jess Divers <jessica.divers@library.ucla.edu>
2026-06-09 15:21:30 -0700

    fix: add a11y checks to all tests

--- cypress/e2e/eventSeriesDetailPage.cy.js
+++ cypress/e2e/eventSeriesDetailPage.cy.js
@@ -11,6 +11,27 @@ function runEventSeriesDetailTests({ withSnapshot = false } = {}) {
       cy.visualSnapshot('eventseriesdetailpage')
     }
   })
+
+  // Use Axe-core to check for critical and serious accessibility violations
+  // To prevent cypress from hanging, we set the retries to 0 and put this test at the end of the tests
+  it('has no accessibility violations', {
+    retries: {
+      runMode: 0,
+      openMode: 0,
+    },
+  }, () => {
+    cy.visit('/series/step-up-series', { failOnStatusCode: false })
+    cy.injectAxe()
+    // add 'moderate' to the includedImpacts array to check for moderate accessibility violations
+    cy.checkA11y('#main', { includedImpacts: ['critical', 'serious'] }, (violations) => {
+      violations.forEach((violation) => {
+        cy.log(`Accessibility Violation: ${violation.id} ${violation.impact} 
+        Description: ${violation.description} 
+        Help: ${violation.help} ${violation.helpUrl} 
+        HTML hint: ${violation.nodes.length} ${violation.nodes[0].html}`)
+      })
+    })
+  })
 }
 
 if (isChromatic) {
--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -38,6 +38,28 @@ function runHomepageTests({ withSnapshot = false, label = 'Desktop' } = {}) {
       cy.visualSnapshot('homePage')
     }
   })
+
+  // Use Axe-core to check for critical and serious accessibility violations
+  // To prevent cypress from hanging, we set the retries to 0 and put this test at the end of the tests
+  it('has no accessibility violations', {
+    retries: {
+      runMode: 0,
+      openMode: 0,
+    },
+  }, () => {
+    cy.visit('/series/step-up-series', { failOnStatusCode: false })
+    cy.injectAxe()
+    // add 'moderate' to the includedImpacts array to check for moderate accessibility violations
+    // check the entire page for accessibility violations, including the header and footer
+    cy.checkA11y(null, { includedImpacts: ['critical', 'serious'] }, (violations) => {
+      violations.forEach((violation) => {
+        cy.log(`Accessibility Violation: ${violation.id} ${violation.impact} 
+        Description: ${violation.description} 
+        Help: ${violation.help} ${violation.helpUrl} 
+        HTML hint: ${violation.nodes.length} ${violation.nodes[0].html}`)
+      })
+    })
+  })
 }
 
 if (isChromatic) {
