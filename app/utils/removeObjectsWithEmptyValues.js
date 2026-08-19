72db37d6b7688a1de087a489a83f1fd4b0dc90b2
pghorpade <pghorpade@library.ucla.edu>
2026-08-19 12:25:54 -0700

    fix: use every as we need to chekc if all values are empty in the obj instead od atleast one having a value

--- app/utils/removeObjectsWithEmptyValues.js
+++ app/utils/removeObjectsWithEmptyValues.js
@@ -1,5 +1,5 @@
 export function removeObjectsWithEmptyValues(arr) {
   return arr.filter(obj =>
-    Object.values(obj).some(value => value !== null && value !== undefined && value !== '')
+    Object.values(obj).every(value => value !== null && value !== undefined && value !== '')
   )
 }
